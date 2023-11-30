5c8abfbd5737a014aebc0f3b696176d7650e59a1
Cristina RP <cristinarp95@gmail.com>
2023-11-30 14:11:28 +0100

    Create Project and Task models

---
+++ models/Project.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const projectSchema = new Schema({
+  title: {
+    type: String,
+    require: true,
+  },
+
+  description: String,
+
+  tasks: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Task",
+    },
+  ],
+
+  // owner will be added later on
+});
+
+module.exports = model("Project", projectSchema);
